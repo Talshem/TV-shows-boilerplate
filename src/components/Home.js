ee75244c93dde01f2e65dd736da212f60bbc7ee5
Talshem <talsh8797@gmail.com>
2020-10-17 19:37:19 +0300

    finished challenged passing tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Show from './Show';
 import './Home.css';
 import bg from '../video/popcorn.mp4';
+import axios from 'axios'
 
 function Home() {
-  const [shows, setShows] = useState([]);
+const [shows, setShows] = useState([]);
+const [search, setSearch] = useState('')
 
-  /*
-      
-    Insert your code here 
-      
-      
-  */
+useEffect(() => {
+const fetchData = async () => {
+await axios.get('https://www.episodate.com/api/most-popular')
+.then((res) => {
+setShows(res.data.tv_shows)
+})
+}; fetchData()
+}, [])
+
+const handleSearch = async (e) => {
+e.preventDefault()
+await axios.get(
+search === '' ? 
+'https://www.episodate.com/api/most-popular'
+:
+`https://www.episodate.com/api/search?q=${search}`
+)
+.then((res) => {
+setShows(res.data.tv_shows)
+})
+}
 
   return (
     <div className='app'>
+<form onSubmit={handleSearch}>
+<input onChange={(e) => setSearch(e.target.value)} id="search-bar"/>
+<button id="submit-btn">Search</button>
+</form>
+
+
+
       <video src={bg} playsInline autoPlay muted loop id='bgvid' />
       {/* If you want to know how to implement video as your background 
       you can take a look here: https://www.w3schools.com/howto/howto_css_fullscreen_video.asp */}
--- src/components/OneShow.js
+++ src/components/OneShow.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import './OneShow.css';
 import likedImg from "../media/liked.png"
 import notLikedImg from "../media/notLiked.png"
+import axios from 'axios'
 
 function OneShow() {
-  const { id } = useParams(); //this is the selected show id
+const { id } = useParams(); //this is the selected show id
+const [show, setShow] = useState(null)
+const [liked, setLiked] = useState(null)
 
-    /*
-      
-      insert your code here 
-      
-      
-    */
+
+useEffect(() => {
+const fetchData = async () => {
+await axios.get(`https://www.episodate.com/api/show-details?q=${id}`)
+.then((res) => {
+setShow(res.data.tvShow)
+if (localStorage.getItem(res.data.tvShow.id)) {
+setLiked(localStorage.getItem(res.data.tvShow.id))
+}
+})
+}; fetchData()
+}, [])
+
+const handleLike = () => {
+if (localStorage.getItem(show.id)) {
+localStorage.removeItem(show.id)
+setLiked(null)
+return
+}
+localStorage.setItem(show.id, likedImg)
+setLiked(localStorage.getItem(show.id))
+}
+
+const ratingColor = (e) => {
+if(e >= 8){
+return 'green'
+} else if (e >= 6){
+return 'yellow'
+}
+return 'red'
+}
+
 
   return (
     <div className='one-show-container'>
@@ -22,13 +51,31 @@ function OneShow() {
           alt='Go back'
           src='https://img.icons8.com/metro/52/000000/circled-left-2.png'
         />
-      </Link>
-      {/*
-      
-        insert your code here 
-      
-      
-      */}
+    </Link>
+
+{show && <>
+<div className="like-div" onClick={handleLike}>
+<img className="interaction-img" alt={liked ? 'liked' : 'not liked'} src={liked ? liked : notLikedImg}/>
+</div>
+
+<div className='one-show-img-and-title'>
+<h2>{show.name}</h2>
+<img className="one-show-img" src={show.image_path}/>
+<div className='one-show-footer'>
+<div className="seasons">{show.episodes[show.episodes.length-1].season} seasons</div>
+<div className="genres">{show.genres.map(e => <span key={e} className="genre">{e}</span>)}</div>
+<div className="rating">
+<span className={ratingColor(Number(show.rating))}>{show.rating.length > 3 ? show.rating.substr(0,3): show.rating}</span>
+</div>
+<div className="status">{show.status}</div>
+</div>
+</div>
+
+<div className='one-show-description'>
+<h2>description:</h2>
+{show.description}
+</div>
+</>}
     </div>
   );
 }
